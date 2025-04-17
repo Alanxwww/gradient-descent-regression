728c4d3ce801122d267ca6d9e4da96acdd542989
Alanxwww <alanwang166@gmail.com>
2025-04-16 17:52:23 -0700

    push

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
+![Convergence Plot](result/convergence_plot.png)
+
 from utils.visualize import plot_regression_line
 plot_regression_line(x, y, model.predict(x), save_path="result/convergence_plot.png")
